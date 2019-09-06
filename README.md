fa0912796040018cd98440d5ba52ed234a14e149
mdw <github@mikeward.net>
2019-09-05 22:41:08 -0400

    added support for SHA3

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 ## shasum.py - create and verify SHA1 file hashes
 This was designed to operate much like the *shasum* program on OSX and freeBSD. 
-That is to say, instead of using sha1sum or sha256sum, simply specify the algorithm to be used.
-Hashes generated are displayed and also saved to file.algo (file.sha1, file.sha256, file.sha512)
+That is to say, instead of using *sha1sum* or *sha256sum*, simply specify the algorithm to be used.
+Hashes generated are displayed and also saved to file.algo (file.sha1, file.sha256, etc.)
+Supported hashing algorithms: SHA1, SHA256, SHA512, SHA3
 
-#### create SHA1, SHA256 or SHA512 hash of a file and write it to a file
-#### compare SHA1, SHA256 or SHA512 hash of a file with a hash or file containing a hash
+usage: shasum.py [-h] [-a {sha1,sha256,sha512,sha3}] [-c COMPARE] filename
+
+hash a file or compare hash to file
+
+positional arguments:
+  filename              file to be hashed
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a {sha1,sha256,sha512,sha3}, --algorithm {sha1,sha256,sha512,sha3}
+                        choose hashing algorithm: sha1 (default), sha256,
+                        sha512, sha3 (256)
+  -c COMPARE, --compare COMPARE
+                        provide a hash to compare
+
 
 ### examples:
 
@@ -14,6 +28,13 @@ $ ./shasum.py file.txt
 #### compare SHA1(file.txt) to previous hash stored in text.sha1
 $ ./shasum.py file.txt -c file.sha1
 $ ./shasum.py file.txt -c 761f51bd951c41b98d6385e0751699bfb9acc0d0
+
+#### create SHA256 hash of file.txt and write to disk as file.sha256
+$ ./shasum -a sha256 file.txt
+
+#### compare SHA3 hash of file.txt to the checksum literal (or from file)
+$ ./shasum -a sha3 file.txt -c 171609e28789aa5d3a30b02d14bae27509dc5228318123c7fec96cab83749dcd
+$ ./shasum -a sha3 file.txt -c file.sha3
 
 #### create SHA256 hash of file.txt, save to file
 $ ./shasum -a 256 checksumfile
--- shasum.py
+++ shasum.py
@@ -3,15 +3,11 @@
 import os
 import argparse
 import hashlib
-from  termcolor import cprint
+from termcolor import cprint
 
 
 class HashFile:
-	""" hash a file (SHA1) or compare to a hash value to verify file integrity
-		to do: support SHA256, SHA512 by using -a switch on command line 
-		this is how shasum program on OSX works 
-		another idea is to automatically look for file.sha1, file.sha256 etc.
-		but not sure what to do if multiple are found """
+	""" hash a file or compare to a hash value to verify file integrity """
 
 	def __init__(self):
 		self.path = None
@@ -20,7 +16,9 @@ class HashFile:
 	def get_args():
 		parser = argparse.ArgumentParser(description="hash a file or compare hash to file")
 		parser.add_argument("filename", help="file to be hashed")
-		parser.add_argument("-a", "--algorithm", type=int, help="choose SHA hashing algorithm: 1 (default), 256, 512")
+		parser.add_argument("-a", "--algorithm", default="sha1",
+							choices=['sha1', 'sha256', 'sha512', 'sha3'],
+							help="choose hashing algorithm: sha1 (default), sha256, sha512, sha3 (256)")
 		parser.add_argument("-c", "--compare", help="provide a hash to compare")
 		return parser.parse_args()
 
@@ -42,17 +40,19 @@ class HashFile:
 
 	def write_hash_file(self, file, algo):
 		"""write hash to a file with same name, sha extension"""
-		file_name = file.split('.')[0] + f".sha{algo}"
+		file_name = file.split('.')[0] + '.' + algo
 		with open(file_name, 'w') as f:
 			f.write(self.compute_hash(file, algo))
 
 
 	def compute_hash(self, file, algo):
 		BUFF_SIZE = 65536
-		if algo == 256:
+		if algo == 'sha256':
 			digest = hashlib.sha256()
-		elif algo == 512:
+		elif algo == 'sha512':
 			digest = hashlib.sha512()
+		elif algo == 'sha3':
+			digest = hashlib.sha3_256()
 		else:
 			digest = hashlib.sha1()
 
@@ -64,7 +64,6 @@ class HashFile:
 					break
 				# keep updating digest until no more chunks of data
 				digest.update(data)
-
 		return digest.hexdigest()
 		
 
@@ -72,7 +71,7 @@ class HashFile:
 		args = self.get_args()
 		self.filename = args.filename
 
-		algo = 1 if not args.algorithm else args.algorithm
+		algo = 'sha1' if not args.algorithm else args.algorithm
 		hash = self.compute_hash(self.filename, algo)
 		
 		if args.compare:
